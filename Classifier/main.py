a31b23749a766dd43417c0fd06f82f4e6c140352
Ansh Sharma <wiredfoxterrier17@gmail.com>
2020-10-06 09:03:01 +0530

    Patch 0.0.1

---
+++ Classifier/main.py
@@ -0,0 +1,12 @@
+#Ansh Sharma
+#Patch-2
+#Date:
+#Changes:
+
+
+import numpy as np #Imports numpy which is used for all types of calculations
+import pandas as pd #Imports Pandas which does the data processing
+
+
+a = np.zeros((2,2))
+print (str(a))
